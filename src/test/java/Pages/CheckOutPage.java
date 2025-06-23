1685f20fa5c043384f46219400a15cfec5c2ce6f
MrWilson <Refilwe.wilson@gmail.com>
2025-06-23 23:10:02 +0200

    Added the firstName,LastName and Postal code method and the submit button

--- src/test/java/Pages/CheckOutPage.java
+++ src/test/java/Pages/CheckOutPage.java
@@ -1,4 +1,42 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class CheckOutPage {
+
+    WebDriver driver;
+
+    @FindBy(id = "first-name")
+    WebElement firstName_id;
+
+    @FindBy(id = "last-name")
+    WebElement lastName_id;
+
+    @FindBy(id = "postal-code")
+    WebElement postalCode_id;
+
+    @FindBy(id = "continue")
+    WebElement continue_id;
+
+    public CheckOutPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterFirstname(String firstname) {
+        firstName_id.sendKeys(firstname);
+    }
+
+    public void enterLastname(String lastname) {
+        lastName_id.sendKeys(lastname);
+    }
+
+    public void enterPostalcode(String postalcode) {
+        postalCode_id.sendKeys(postalcode);
+    }
+
+    public void submitButton() {
+        continue_id.click();
+    }
 }
