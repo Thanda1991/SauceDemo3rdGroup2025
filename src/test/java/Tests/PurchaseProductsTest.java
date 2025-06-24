01ed0009d32b1a35ee7d118af752f4e0f2deccb3
cmokale <cideebay@gmail.com>
2025-06-24 11:36:29 +0200

    Created ClickAddToCart class in PurchaseProductsTest page Created ClickCart class in PurchaseProductsTest page Created CartPage

--- src/test/java/Tests/PurchaseProductsTest.java
+++ src/test/java/Tests/PurchaseProductsTest.java
@@ -28,8 +28,20 @@ public class PurchaseProductsTest extends Base{
         homePage.verifyProductTitle();
     }
 
-    @AfterTest
+
+    @Test(dependsOnMethods = "verifyProductTitle")
+    public void clickAddToCart()
+    {
+        homePage.clickAddToCart();
+    }
+    @Test(dependsOnMethods = "clickAddToCart")
+    public void ClickCart()
+    {
+        homePage.ClickCart();
+    }
+
+    /*@AfterTest
     public void closeBrowser() {
         driver.quit();
-    }
+    }*/
 }
