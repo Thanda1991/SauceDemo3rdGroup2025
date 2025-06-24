14fbfc65503c46d9a1272b3bfbeef6047786e045
cmokale <cideebay@gmail.com>
2025-06-24 16:51:14 +0200

    Created CartTitle_xpath class

--- src/test/java/Pages/CartPage.java
+++ src/test/java/Pages/CartPage.java
@@ -3,10 +3,23 @@ package Pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class CartPage {
     WebDriver driver;
 
     @FindBy(xpath = "//span[contains(.,'Your Cart')]")
-    WebElement cartTitle_xpath;
+    WebElement CartTitle_xpath;
+
+    public CartPage (WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void verifyCartTitle() {
+        new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOf(CartTitle_xpath));
+        CartTitle_xpath.isDisplayed();
+    }
 }
