330c527fa01d1e0a1aab2d0264471f9c150f94ea
cmokale <cideebay@gmail.com>
2025-06-24 20:03:08 +0200

    Created backToProductButton class

--- src/test/java/Pages/CompletePage.java
+++ src/test/java/Pages/CompletePage.java
@@ -13,8 +13,8 @@ public class CompletePage {
 
     @FindBy(xpath = "//h2[contains(.,'Thank you for your order!')]")
     WebElement checkOutComplete_xpath;
-    /*@FindBy(xpath = "//h2[contains(.,'Thank you for your order!')]")
-    WebElement checkOutComplete_xpath;*/
+    @FindBy(id = "back-to-products")
+    WebElement backToProducts_id;
 
     public CompletePage (WebDriver driver) {
         this.driver = driver;
@@ -23,5 +23,9 @@ public class CompletePage {
     public void verifyCompleteCheckOut(){
         new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOf(checkOutComplete_xpath));
         checkOutComplete_xpath.isDisplayed();
+    }
+
+    public void backToProductButton(){
+        backToProducts_id.click();
     }
 }
