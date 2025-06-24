90119177434681bc2639c2c14ed778779749bb79
cmokale <cideebay@gmail.com>
2025-06-24 17:23:03 +0200

    Created InfoTitle_xpath class

---
+++ src/test/java/Pages/InformationPage.java
@@ -0,0 +1,27 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class InformationPage {
+    WebDriver driver;
+
+    @FindBy(xpath = "//span[contains(.,'Checkout: Your Information')]")
+    WebElement InfoTitle_xpath;
+
+    public InformationPage (WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void verifyCartTitle() {
+        new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOf(InfoTitle_xpath));
+        InfoTitle_xpath.isDisplayed();
+    }
+}
+
+
