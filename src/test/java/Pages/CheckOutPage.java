b361608d8cf385e54aeae2a37dc14dbd4f33eaa6
cmokale <cideebay@gmail.com>
2025-06-24 19:42:32 +0200

    Created verifyCheckoutTitle class

---
+++ src/test/java/Pages/CheckOutPage.java
@@ -0,0 +1,25 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class CheckOutPage {
+    WebDriver driver;
+
+    @FindBy(xpath = "//span[contains(.,'Checkout: Overview')]")
+    WebElement checkOut_xpath;
+
+    public CheckOutPage (WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void verifyCheckoutTitle() {
+        new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOf(checkOut_xpath));
+        checkOut_xpath.isDisplayed();
+    }
+}
