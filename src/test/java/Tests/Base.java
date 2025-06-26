d06ce53df17d3f36c45cdec483ef7940e60f1839
Amanda <simangeleramavhale@gmail.com>
2025-06-26 20:30:39 +0200

    We combined Amanda_playground's code with the main branch code

--- src/test/java/Tests/Base.java
+++ src/test/java/Tests/Base.java
@@ -3,13 +3,12 @@ package Tests;
 import Pages.*;
 import Utils.BrowserFactory;
 import Utils.ReadFromFile;
-import Utils.TakesScreenshots;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class Base {
     BrowserFactory browserFactory = new BrowserFactory();
-    final WebDriver driver = browserFactory.startBrowser("chrome", "https://www.saucedemo.com/");
+    final WebDriver driver = browserFactory.startBrowser("edge", "https://www.saucedemo.com/");
 
     LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
     HomePage homePage = PageFactory.initElements(driver, HomePage.class);
@@ -17,10 +16,8 @@ public class Base {
     CheckOutInformationPage checkOutInformationPage = PageFactory.initElements(driver, CheckOutInformationPage.class);
     CheckOutOverViewPage checkOutOverViewPagePage = PageFactory.initElements(driver, CheckOutOverViewPage.class);
     CheckOutThankYouPage checkOutThankYouPage = PageFactory.initElements(driver, CheckOutThankYouPage.class);
-
-
-
-    CheckoutPage checkoutPage = PageFactory.initElements(driver, CheckoutPage.class);
     ReadFromFile readFromFile = new ReadFromFile();
-    TakesScreenshots takesScreenshots = new TakesScreenshots();
+
+
+
 }
