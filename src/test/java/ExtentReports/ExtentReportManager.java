c456d5e47e76d27c14f4597c5a563d453b25d157
Ourmine1010 <neo.ntwayaborwa@gmail.com>
2025-07-03 18:51:17 +0200

    Add Extend Reports module

---
+++ src/test/java/ExtentReports/ExtentReportManager.java
@@ -0,0 +1,30 @@
+package ExtentReports;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import java.io.File;
+
+public class ExtentReportManager {
+
+    private static String reportDir=System.getProperty("user.dir") + "/Reports/sauceDemo.html/";
+
+    private static ExtentReports extentReports;
+
+    private static ExtentSparkReporter extentSparkReporter;
+
+    public static ExtentReports extentSetup(){
+
+        extentReports = new ExtentReports();
+        extentSparkReporter = new ExtentSparkReporter(new File(reportDir));
+        extentReports.attachReporter(extentSparkReporter);
+
+        extentSparkReporter.config().setDocumentTitle("Extent Report");
+        extentSparkReporter.config().setReportName("Sauce Demo");
+
+        extentReports.setSystemInfo("os", System.getProperty("os.name"));
+        extentReports.setSystemInfo("Exection Machine", System.getProperty("user.name"));
+        return extentReports;
+    }
+
+}
