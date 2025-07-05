c1476148f2716acd24abd4d084b3b8feba56b240
Amanda <simangeleramavhale@gmail.com>
2025-07-05 20:55:27 +0200

    created ExtentReportManager class and listener

---
+++ src/test/java/ExtentReports/Listener.java
@@ -0,0 +1,13 @@
+package ExtentReports;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import org.testng.ITestListener;
+
+public class Listener implements ITestListener {
+
+    private static ExtentReports exyent;
+    private  static ExtentTest extentTest;
+
+
+}
