d52c49330a9579bd123d65da85c7f1338e689e87
bmahinga <shiluvam@gmail.com>
2025-07-06 17:06:31 +0200

    Deleted and Added New classes

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,4 +1,31 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
+
+
+    WebDriver driver;
+
+    @FindBy(id = "user-name")
+    WebElement username_id;
+
+    @FindBy(id = "password")
+    WebElement password_id;
+
+    @FindBy(id = "login-button")
+    WebElement loginButton_id;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+
+
+
+
+
 }
