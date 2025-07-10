428b4515e5987599f6f5a5a8f40241eb2733766e
bmahinga <shiluvam@gmail.com>
2025-07-10 10:42:31 +0200

    Deleted and Added New classes. added more code on the created classes added VAT calc

--- src/test/java/Pages/CheckoutOverview.java
+++ src/test/java/Pages/CheckoutOverview.java
@@ -17,28 +17,6 @@ public class CheckoutOverview {
     @FindBy(xpath = "//button[@id='finish']")
     WebElement finishButton_xpath;
 
-    @FindBy(xpath = "//div[@id='checkout_summary_container']/div/div[2]/div[6]")
-    WebElement balance_Label_xpath;
-
-    public void verifyCheckOutLabel() {
-        new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOf(checkoutLabel_xpath));
-        checkoutLabel_xpath.isDisplayed();
-    }
-
-    public void verifybalance() { //Show VAT and Grand Total on the page
-        double subtotal = Double.parseDouble(balance_Label_xpath.getText());
-
-        // Calculate VAT (8%) and Grand Total
-        double vatPercent = 0.08;
-        double vat = subtotal * vatPercent / 100;
-        double grandTotal = subtotal + vat;
-
-        System.out.println("Selected Price: $" + subtotal);
-        System.out.println("VAT (8%): $" + vat);
-        System.out.println("Grand Total: $" + grandTotal);
-
-
-    }
     public void clickFinishButton() {
         finishButton_xpath.click();
     }
--- src/test/java/Pages/CheckoutPage.java
+++ src/test/java/Pages/CheckoutPage.java
@@ -5,49 +5,78 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 
 public class CheckoutPage {
     WebDriver driver;
 
-    @FindBy(xpath="//span[contains(.,'Checkout: Your Information')]")
+    @FindBy(xpath = "//span[contains(.,'Checkout: Your Information')]")
     WebElement yourInformationLabel;
 
-    @FindBy(id="first-name")
+    @FindBy(id = "first-name")
     WebElement firstNameYourInformation;
 
-    @FindBy(id="last-name")
+    @FindBy(id = "last-name")
     WebElement lastNameYourInformation;
 
-    @FindBy(id="postal-code")
+    @FindBy(id = "postal-code")
     WebElement postCodeYourInformation;
 
-    @FindBy(id="continue")
+    @FindBy(id = "continue")
     WebElement continueButton;
+
+    @FindBy(xpath = "//div[@id='checkout_summary_container']/div/div[2]/div[6]")
+    WebElement balance_Label_xpath;
+
+    @FindBy(xpath = "xpath=//div[@id='checkout_summary_container']/div/div[2]/div[7]")
+    WebElement vat_label_xpath;
+
+    @FindBy(xpath = "//div[@id='checkout_summary_container']/div/div[2]/div[8]")
+    WebElement grand_total_label_xpath;
+
+
     //checking if i am on the shipping details page
-    public void verifyYourInformationLabel(){
+    public void verifyYourInformationLabel() {
         new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOf(yourInformationLabel));
         yourInformationLabel.isDisplayed();
     }
+
     //methods that will add my shipping details
-    public void enterFirstNameYourInformation(String FirstName){
+    public void enterFirstNameYourInformation(String FirstName) {
 
         firstNameYourInformation.sendKeys(FirstName);
     }
 
-    public void enterLastNameYourInformation(String LastName){
+    public void enterLastNameYourInformation(String LastName) {
 
         lastNameYourInformation.sendKeys(LastName);
     }
 
-    public void enterPostCodeYourInformation(String PostCode){
+    public void enterPostCodeYourInformation(String PostCode) {
 
         postCodeYourInformation.sendKeys(PostCode);
     }
-    public void clickContinueButton(){
+
+    public void clickContinueButton() {
 
         continueButton.click();
     }
+
+    public double getTheAmount(WebElement element) {
+        String itemCostText = element.getText();
+        String[] splitTex = itemCostText.split("\\$");
+        return Double.parseDouble(splitTex[1]);
+    }
+
+    public void VatCalc() {
+        double sum = getTheAmount(balance_Label_xpath) + getTheAmount(vat_label_xpath);
+        double totalAmount = Math.floor(sum * 100) / 100;
+        double actualTotal = getTheAmount(grand_total_label_xpath);
+        Assert.assertEquals(totalAmount, actualTotal);
+        System.out.println("Expected amount:" + totalAmount + "=" + "Actual amount");
+    }
+
 
 }
