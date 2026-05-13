a1b8d7de53b8b1916ec86dabe9777dc98495ce12
lubabalo.mkhize <Lubabalo.Mkhize@gmail.com>
2026-05-13 13:28:06 +0200

    Updated Base class.

--- src/test/java/Utils/ReadFromFile.java
+++ src/test/java/Utils/ReadFromFile.java
@@ -3,13 +3,14 @@ package Utils;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class ReadFromFile {
 
-    private static String testData = System.getProperty("user.dir")+"/src/test/java/TestData/data.xlsx";
+    private static String testData = System.getProperty("user.dir") + "/src/test/java/TestData/data.xlsx";
 
     static FileInputStream fs;
 
@@ -37,20 +38,20 @@ public class ReadFromFile {
 
     public static String password = sheet.getRow(1).getCell(1).getStringCellValue();
 
-   static XSSFSheet sheet2 = workbook.getSheet("user_details");
+    static XSSFSheet sheet2 = workbook.getSheet("user_details");
 
-   public static String firstName = sheet2.getRow(1).getCell(0).getStringCellValue();
+    public static String firstName = sheet2.getRow(1).getCell(0).getStringCellValue();
 
-   public static String lastName = sheet2.getRow(1).getCell(1).getStringCellValue();
+    public static String lastName = sheet2.getRow(1).getCell(1).getStringCellValue();
 
-   public static String postalCode = sheet2.getRow(1).getCell(2).getRawValue();
+    public static String postalCode = sheet2.getRow(1).getCell(2).getRawValue();
 
     @Test
-    public void test(){
-        System.out.println("username is "+username);
-        System.out.println("password is "+password);
-        System.out.println("username is "+firstName);
-        System.out.println("password is "+lastName);
-        System.out.println("password is "+postalCode);
+    public void test() {
+        System.out.println("username is " + username);
+        System.out.println("password is " + password);
+        System.out.println("username is " + firstName);
+        System.out.println("password is " + lastName);
+        System.out.println("password is " + postalCode);
     }
 }
